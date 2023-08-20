eb0f9d297de29de2bf1c0cf64b844c8ada9749aa
Marko Macura <mmacura9@Markos-MacBook-Air.local>
2023-08-20 23:32:00 +0100

    Add more tests

---
+++ repository/main_test.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ChooseCruise/choosecruise-backend/bootstrap"
+	db "github.com/ChooseCruise/choosecruise-backend/db/sqlc"
+	"github.com/ChooseCruise/choosecruise-backend/domain"
+	_ "github.com/lib/pq"
+)
+
+var testStore db.Store
+var userRep domain.UserRepository
+
+func TestMain(m *testing.M) {
+	env := bootstrap.LoadEnv("..")
+	conn, err := sql.Open(env.DBDriver, env.DBSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db")
+	}
+
+	testStore = db.NewStore(conn)
+	userRep = NewUserRepository(testStore)
+
+	os.Exit(m.Run())
+}
