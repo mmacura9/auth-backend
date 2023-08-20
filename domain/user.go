7074ee86da1172eb375d160bdafc7a7ee15bdcf8
Marko Macura <mmacura9@Markos-MacBook-Air.local>
2023-08-20 16:21:17 +0100

    Add gitignore

--- domain/user.go
+++ domain/user.go
@@ -21,5 +21,4 @@ type UserRepository interface {
 	Create(c context.Context, user *User) error
 	Fetch(c context.Context) ([]User, error)
 	GetByEmail(c context.Context, email string) (User, error)
-	GetByID(c context.Context, id string) (User, error)
 }
